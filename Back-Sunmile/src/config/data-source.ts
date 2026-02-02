d84e19c4a8aff73c7e78691596a4a268d411a3f3
Felipe <felipeccantini@gmail.com>
2026-02-01 23:14:25 -0300

    Synchronize não funciona no vercel :/

--- Back-Sunmile/src/config/data-source.ts
+++ Back-Sunmile/src/config/data-source.ts
@@ -9,16 +9,12 @@ const { DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_NAME } = process.env
 
 export const AppDataSource = new DataSource({
   type: 'mysql',
-  host: DB_HOST,
-  port: Number(DB_PORT || '3306'),
-  username: DB_USERNAME,
-  password: DB_PASSWORD,
-  database: DB_NAME,
-  synchronize: true,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT || 3306),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  synchronize: process.env.NODE_ENV !== 'production',
   logging: true,
-  entities: [
-    process.env.NODE_ENV === 'production'
-      ? path.join(__dirname, '../models/*.js') 
-      : path.join(__dirname, '../models/*.ts') 
-  ],
+  entities: [path.join(__dirname, '..', 'models', '*.{js,ts}')],
 })
